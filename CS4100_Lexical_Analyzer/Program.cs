1e5aab1ddb309232bd9f5bf508058e89a9844af6
Daniel Fosha <danfosha@gmail.com>
2017-09-26 08:46:20 -0600

    Add project files.

---
+++ CS4100_Lexical_Analyzer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4100_Lexical_Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeStructures();
+            InitializeInputFile(GetFileName);
+            while (!EOF)
+            {
+                GetNextToken(echoOn);
+                PrintToken(nextToken, tokenCode);
+            }
+            SymbolTable.Print();
+            Terminate;
+            
+        }
+
+        public void InitializeStructures()
+        {
+            int MaxQuad = 100;            
+            SymbolTable Symbols1 = new SymbolTable(MaxQuad);
+        }
+
+    }
+
+  
+}
