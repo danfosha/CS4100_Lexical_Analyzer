ee4e0d9cc3515159a4cdf88120c51ed0d5d80ac0
Daniel F. Fosha <danfosha@gmail.com>
2017-10-31 11:32:26 -0600

    Added another test file

--- CS4100_Lexical_Analyzer/Program.cs
+++ CS4100_Lexical_Analyzer/Program.cs
@@ -17,7 +17,8 @@ namespace CS4100_Lexical_Analyzer
             bool echoOn = true;
             // string fileName = "GoodtreeA.txt";
             // string fileName = "BadProg1.txt";
-            string fileName = "BadProg2B.txt";
+            // string fileName = "BadProg2B.txt";
+            string fileName = "BadProg3B.txt";
             InitializeStructures();
             FileHandler.InitializeInputFile(fileName);
             PrintHeader();
