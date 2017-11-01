a0c107bda2d225407238cd063407c26f69ccd9a9
Daniel F. Fosha <danfosha@gmail.com>
2017-10-31 20:49:46 -0600

    Updated \r handling

--- CS4100_Lexical_Analyzer/Tokenizer.cs
+++ CS4100_Lexical_Analyzer/Tokenizer.cs
@@ -131,7 +131,7 @@ namespace CS4100_Lexical_Analyzer
                 else if (!tokenComplete)
                 {
 
-                    while (Char.IsWhiteSpace(nextChar))
+                    while (' '.Equals(nextChar)) 
                     {
                         nextChar = GetNextChar();
                     }
@@ -160,6 +160,10 @@ namespace CS4100_Lexical_Analyzer
                     else if (OtherTokenSecond(nextChar))
                     {
                         caseGroup = 6;
+                    }
+                    else if ('\r'.Equals(nextChar))
+                    {
+                        caseGroup = 7;
                     }
                     else
                     {
@@ -403,6 +407,8 @@ namespace CS4100_Lexical_Analyzer
                                 tokenComplete = true;
                             }
                             break;
+                        case 7:
+                            break;
                         default:
                             break;
                     }
@@ -524,6 +530,7 @@ namespace CS4100_Lexical_Analyzer
             {
                 case 0:
                     // unidentified
+                    if (!("".Equals(token)))
                     tokenCode = 99;
                     break;
                 case 1:
